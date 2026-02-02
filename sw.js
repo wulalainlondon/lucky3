ccf9e9843e8f0980ce48838b9c4952f6bc088bff
Wulala <wulala@WulaladeMacBook-Air.local>
2026-02-03 00:31:28 +0800

    Initialize PWA and add assets

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'lucky3-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './Lucky3.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
